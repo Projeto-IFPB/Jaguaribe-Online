0ae394af62c35a6e068a3eb616db971dbb4b8f1a
Lemuel <lemuelduartep@gmail.com>
2025-12-10 23:26:09 -0300

    feat/navbar-responsivo-e-redirecionando-para-url-certa

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
     return render_template("index.html")
+
+@app.route("/produtos")
+def pagina_produtos():
+    return render_template("produtos.html")
+
+@app.route("/login")
+def pagina_login():
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
