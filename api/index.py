0bcf0df6c7b9f940169f7a412a3c173ec3e2bbfa
Ryan Enriq <ryanenriq.dev@gmail.com>
2026-05-09 11:23:35 -0300

    alterações para o vercel funcionar

--- api/index.py
+++ api/index.py
@@ -1,2 +1,9 @@
-from app.main import app
-# A Vercel vai procurar pelo objeto 'app' aqui
+import sys
+import os
+
+# Adiciona a raiz ao path para encontrar o main.py
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from main import create_app
+
+app = create_app()
